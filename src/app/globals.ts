9b16d431176b16123d815160dee99322aede992e
nadeesh-garg <nadeesh.garg@gmail.com>
2018-08-01 14:58:44 +0530

    Backend URL changes

--- src/app/globals.ts
+++ src/app/globals.ts
@@ -2,6 +2,6 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class Global{
-	public static BACKEND_URL = "http://localhost:8000";
+	public static BACKEND_URL = "http://18.191.158.152:8000";
 	someID: string = '';
 }
